f696777c3a020c7022bbec13f5a9608358488d38
surya <surya@LAPTOP-OIRVIP80>
2023-10-31 19:17:33 +0530

    1

---
+++ MIDTERM/src/test/java/surya/text.java
@@ -0,0 +1,18 @@
+package surya;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class test {
+    @Test
+    public void logtest1() {
+        app obj = new app();
+        Assert.assertEquals(1, obj.log("Ramcharan", 13));
+    }
+
+    @Test
+    public void logtest2() {
+        app obj = new app();
+        Assert.assertEquals(0, obj.log("ram", 3025));
+    }
+}
